9d4e6d2c4f3a7cdd2f63fa28c8079a7541161684
QiuQQ151 <18420171908@163.com>
2026-01-27 23:04:33 +0800

    修改执行脚本

--- html2epub.sh
+++ html2epub.sh
@@ -25,15 +25,15 @@ cp lib/mimetype      temp/xhrb/epub/
 cp lib/container.xml temp/xhrb/epub/META-INF/
 cp lib/kedaya.jpg     temp/xhrb/epub/OEBPS/cover.jpg 
 
-# 执行抓取与epub制作程序
-DIR=$(pwd)
-cd "$DIR/html2epub"
+# 执行抓取
 ./html2epub.exe
 
+#epub制作程序
+DIR=$(pwd)
 # epub打包
 # 选择封面图片
-# largest_image=$(find "$DIR/html2epub/temp/xhrb/epub/OEBPS/images/" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" \) -exec du -b {} + | sort -n | tail -n 1 | awk '{print $2}')
-# cp "$largest_image" "$DIR/html2epub/temp/xhrb/epub/OEBPS/cover.jpg"
+# largest_image=$(find "$DIR/temp/xhrb/epub/OEBPS/images/" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" \) -exec du -b {} + | sort -n | tail -n 1 | awk '{print $2}')
+# cp "$largest_image" "$DIR/temp/xhrb/epub/OEBPS/cover.jpg"
 #     # 特别的封面
 #         # 定义目标日期
 #         target_day=
@@ -45,24 +45,22 @@ cd "$DIR/html2epub"
 #         if [ "$today_day" -eq "$target_day" ] && [ "$today_month" -eq "$target_month" ]; then
 #             echo "今天是 ${target_month}月${target_day}日，执行特定命令。"
 #             # 在这里执行你的特定命令
-#             cp "$DIR/html2epub/lib/birthday.jpg" "$DIR/html2epub/temp/xhrb/epub/OEBPS/cover.jpg"
+#             cp "$DIR/lib/birthday.jpg" "$DIR/temp/xhrb/epub/OEBPS/cover.jpg"
 #         else
 #             echo "今天不是 ${target_month}月${target_day}日。"
 #         fi
-for txt_file in $DIR/html2epub/temp/xhrb/*.txt; do
+for txt_file in $DIR/temp/xhrb/*.txt; do
 filename=$(basename "$txt_file" .txt)  # 如 新华日报20241016
-epub_file="$DIR/html2epub/temp/xhrb/${filename}.epub"
-cd "$DIR/html2epub/temp/xhrb/epub/"
+epub_file="$DIR/temp/xhrb/${filename}.epub"
+cd "$DIR/temp/xhrb/epub/"
 zip -X0 "$epub_file" mimetype
 zip -rX9 "$epub_file" META-INF/ OEBPS/
 done
 
-# largest_image=$(find "$DIR/html2epub/temp/nfrb/epub/OEBPS/images/" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" \) -exec du -b {} + | sort -n | tail -n 1 | awk '{print $2}')
-# cp "$largest_image" "$DIR/html2epub/temp/nfrb/epub/OEBPS/cover.jpg"
-for txt_file in $DIR/html2epub/temp/nfrb/*.txt; do
+for txt_file in $DIR/temp/nfrb/*.txt; do
 filename=$(basename "$txt_file" .txt)  # 如 新华日报20241016
-epub_file="$DIR/html2epub/temp/nfrb/${filename}.epub"
-cd "$DIR/html2epub/temp/nfrb/epub/"
+epub_file="$DIR/temp/nfrb/${filename}.epub"
+cd "$DIR/temp/nfrb/epub/"
 zip -X0 "$epub_file" mimetype
 zip -rX9 "$epub_file" META-INF/ OEBPS/
 done
